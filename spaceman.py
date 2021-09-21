2a9e7694bdc18c857516ba8d6ffdf1f9e4e6b04b
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2021-09-21 12:15:54 -0700

    Update

--- spaceman.py
+++ spaceman.py
@@ -41,31 +41,39 @@ def spaceman(secret_word):
     gameIsWin = False
     incorrectGuesses = 0
     guessedLetters = []
+    allGuessed = []
     for i in range(len(secret_word)):
         guessedLetters.append("_")
 
     while(not gameIsOver):
+        print("----------------")
         guess = input("Guess a letter: ")
-        if isInWord(secret_word, guess):
+        if isInWord(toString(allGuessed), guess):
+            print(f"Already guessed: {guess}")
+        elif isInWord(secret_word, guess):
             indexesOfLetter = getIndexsOf(secret_word, guess)
             for i in indexesOfLetter:
                 guessedLetters[i] = guess
             if toString(guessedLetters) == secret_word:
                 gameIsOver = True
                 gameIsWin = True
+            print(f"Guess: {toString(guessedLetters)}\n{7 - incorrectGuesses} guesses left")
         else:
+            print("Guess Incorrect")
             incorrectGuesses += 1
+            print(f"Guess: {toString(guessedLetters)}\n{7 - incorrectGuesses} guesses left")
             if incorrectGuesses == 7:
                 gameIsOver = True
-        print(f"Guess: {toString(guessedLetters)}\n{7 - incorrectGuesses} guesses left")
+        allGuessed.append(guess)
 
     if gameIsWin:
         print("You Win")
     else:
-        print("You Lose")
+        print(f"You Lose the word was {secret_word}")
 
 
 #These function calls that will start the game
 secret_word = load_word()
 print(secret_word)
 spaceman(secret_word)
+
